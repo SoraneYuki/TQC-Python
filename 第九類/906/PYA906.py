c670b1bd282a55139e3ec8ea42e0fac679ccfbd6
SoraneYuki <39622241+SoraneYuki@users.noreply.github.com>
2021-11-28 15:46:30 +0800

    v20211128

--- 第九類/906/PYA906.py
+++ 第九類/906/PYA906.py
@@ -2,10 +2,13 @@ f_name = input()
 str_old = input()
 str_new = input()
 
-
+with open(f_name, "r") as file:
+    data = file.read()
 
 print("=== Before the replacement")
-#TODO
+print(data)
+
+data = data.replace(str_old, str_new)
 
 print("=== After the replacement")
-#TODO
+print(data)
